1b3a41e44109d351f70c74c4fe7df5f89af55ce8
SokolovVasily <ber-sokolov-vasily@outlook.de>
2023-06-26 21:40:46 +0200

    HW

---
+++ HW2.java
@@ -0,0 +1,18 @@
+package org.example.homework;
+
+public class HM2 {
+    public static void main(String[] args) {
+
+            int number = 345;
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number % 10;
+
+            System.out.println("number " + number + " -> " + digit1 + ", " + digit2 + ", " + digit3);
+
+
+
+    }
+
+
+}
