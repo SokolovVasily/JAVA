3cf9aa51840d81167314c3c0a4157f43a5598b9c
SokolovVasily <ber-sokolov-vasily@outlook.de>
2023-06-26 21:39:19 +0200

    HW

---
+++ HW1.java
@@ -0,0 +1,60 @@
+package org.example.homework;
+
+public class HW1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello");
+        char Char = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436F;
+        double myDouble = 4.355453532;
+        long myLong = 12121;
+
+
+        System.out.println("char:"+ Char);
+        System.out.println("int:"+ myInt);
+        System.out.println("byte:"+ myByte);
+        System.out.println("short:"+ myShort);
+        System.out.println("float:"+ myFloat);
+        System.out.println("double:"+ myDouble);
+        System.out.println("long:"+ myLong);
+
+
+
+
+
+        System.out.println("Character = " + Char);
+        System.out.println("Integer = " + myInt);
+        System.out.println("Byte =" + myByte);
+        System.out.println("Short = " + myShort);
+        System.out.println("Float = " + myFloat);
+        System.out.println("Double= " +myDouble);
+        System.out.println("Long = " + myLong);
+
+        Character myCharacter = 'G';
+        Integer myInteger = 89;
+        Byte myByte1 = 4;
+        Short myShort2 = 56;
+        Float myFloat1 = 4.7333436F;
+        Double myDouble1 = 4.355453532;
+        Long myLong1 = 12121L;
+
+
+        System.out.println( Char);
+        System.out.println( myInt);
+
+        System.out.println(myByte);
+        System.out.println( myShort);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myLong);
+
+        System.out.println(myFloat);
+        System.out.println( myDouble);
+
+
+    }
+}
